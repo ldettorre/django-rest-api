7963b936dc8db438240e6d30f2abde970fd19497
Luca D'Ettorre <luca@macbook-pro.home>
2021-03-14 01:15:46 +0000

    I created the test for creating a user with an email in test_models.py, I then created the UserManager and User class in core/models.py.

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
+                                        PermissionsMixin
 
-# Create your models here.
+# AbstractBaseUser provides the core implementation of a user model,
+# including hashed passwords and tokenized password resets.
+# PermissionsMixin is an abstract model that will give us all the methods
+# and db fields to support Djangos permission model.
+
+
+class UserManager(BaseUserManager):
+
+    def create_user(self, email, password=None, **extra_fields):
+        '''This creates and saves a new user.'''
+        '''**extra_fields allows us to include additional fields later on'''
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    ''' This is a Custom User Model that supports email instead of username.'''
+    email = models.EmailField(max_length=250, unique=True)
+    name = models.CharField(max_length=250)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
