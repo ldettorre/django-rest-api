ce0c9c8756045d7a3174e606bc5484738eda1d5d
Luca D'Ettorre <ldettorre21@gmail.com>
2021-03-20 01:30:10 +0000

    Refactored the views in the recipe app using a base viewset

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -61,3 +61,14 @@ class ModelTests(TestCase):
             name="Cucumber"
         )
         self.assertEqual(str(ingredient), ingredient.name)
+
+    def test_recipe_str(self):
+        '''test the recipe string representation'''
+        recipe = models.Recipe.objects.create(
+            user=sample_user(),
+            title='Pasta Bake',
+            time_minutes=5,
+            price=5.00,
+        )
+
+        self.assertEqual(str(recipe), recipe.title)
