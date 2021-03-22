399f862e54c66a564008a7cf4a18f64dbf12bc8d
Luca D'Ettorre <ldettorre21@gmail.com>
2021-03-22 01:02:43 +0000

    Added pillow to requirements.txt for image storage, added the static and media urls and roots to settings.py and also added the static url. Dockerfile was updated for image processing

--- app/app/settings.py
+++ app/app/settings.py
@@ -127,6 +127,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = '/vol/web/media'
+STATIC_ROOT = '/vol/web/static'
 
 # Core refers to our app and User refers to the model within the app. 
 AUTH_USER_MODEL = 'core.User'
